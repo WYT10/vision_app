e703a42048f05b62cd33d9f73d54a136d76b2a79
Tom_4997 <60837240+WYT10@users.noreply.github.com>
2026-03-15 00:37:08 +0800

    gpt update

--- include/app_calib.hpp
+++
@@ -1,6 +0,0 @@
-#ifndef APP_CALIB_HPP
-#define APP_CALIB_HPP
-#include "app_config.hpp"
-
-void run_calib_mode(AppConfig &config);
-#endif
--- include/app_test.hpp
+++
@@ -1,6 +0,0 @@
-#ifndef APP_TEST_HPP
-#define APP_TEST_HPP
-#include "app_config.hpp"
-
-void run_test_mode(const AppConfig &config, bool no_ui);
-#endif
